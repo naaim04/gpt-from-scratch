8a8bc31b37cc351a0adaa80f9cb81a8db74b2d3e
Farhan Naim <nahraf.mian06@gmail.com>
2026-08-02 20:46:44 -0600

    implement multi-head attentin

--- v2.py
+++ v2.py
@@ -55,6 +55,15 @@ def estimate_loss():
     model.train()
     return out
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+    
+
 class Head(nn.Module):
     
     def __init__(self, head_size):
@@ -75,14 +84,12 @@ class Head(nn.Module):
         out = wei @ v
         return out
 
-
-
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -91,7 +98,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
         x = tok_emb + pos_emb
-        x = self.sa_head(x)
+        x = self.sa_heads(x)
         logits = self.lm_head(x)
         if targets is None:
             loss = None
